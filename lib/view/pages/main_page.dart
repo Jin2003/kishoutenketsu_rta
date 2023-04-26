7f145dcfd3b3e62b8f6fa8c34a19a4be641edc42
Jin Sakura <sakura0419gen@icloud.com>
2023-04-26 19:29:10 +0900

    [fix] alarm time display

--- lib/view/pages/main_page.dart
+++ lib/view/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:kishoutenketsu_rta/view/pages/components/custom_text_blue.dart';
 import 'package:kishoutenketsu_rta/view/pages/components/elevate_button.dart';
 import '../constant.dart';
@@ -12,44 +13,85 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  late TimeOfDay _timeOfDay;
+
+  @override
+  void initState() {
+    _timeOfDay = TimeOfDay(hour: 0, minute: 0);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Constant.subColor,
       body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              OutlineButton(
-                title: "8 : 40",
-                width: 300,
-                height: 110,
-                fontsize: 60,
-                shape: 100,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, 
+        children: [
+          Container(
+                alignment: Alignment.center,
+                width: 260,
+                height: 90,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Constant.mainColor, width: 5),
+                  borderRadius: BorderRadius.circular(60),
+                  // color と boxdecorationの共存はNG
+                  color: Constant.white,
+                ),
+                child: Center(
+                  child: SizedBox(
+                    width: 240,
+                    height: 120,
+                    child: Text(
+                      textAlign: TextAlign.center,
+                      '${_timeOfDay.hour.toString().padLeft(2, '0')}:${_timeOfDay.minute.toString().padLeft(2, '0')}',
+                      style: GoogleFonts.zenMaruGothic(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 60,
+                        color: Constant.mainColor,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-              SizedBox(
-                width: 0,
-                height: 30,
-              ),
-              ElevateButton(
-                title: "時刻設定",
-                width: 180,
-                height: 55,
-                fontSize: 20,
-                shape: 30,
-              ),
-              SizedBox(
-                width: 0,
-                height: 20,
-              ),
-              ElevateButton(
-                title: "アラーム音設定",
-                width: 180,
-                height: 55,
-                fontSize: 20,
-                shape: 30,
-              )
-            ]),
+          // OutlineButton(
+          //   //timeOfDayを18:00のような時間を表示する
+          //   title:
+          //       '${_timeOfDay.hour.toString().padLeft(2, '0')}:${_timeOfDay.minute.toString().padLeft(2, '0')}',
+          //   width: 300,
+          //   height: 110,
+          //   fontsize: 60,
+          //   shape: 100,
+          // ),
+          SizedBox(
+            width: 0,
+            height: 30,
+          ),
+          ElevateButton(
+            title: "時刻設定",
+            width: 180,
+            height: 55,
+            fontSize: 20,
+            shape: 30,
+            //時刻の設定をする処理
+            onPressed: () async {
+              final TimeOfDay? timeOfDay = await showTimePicker(
+                  context: context, initialTime: _timeOfDay);
+              if (timeOfDay != null) setState(() => {_timeOfDay = timeOfDay});
+            },
+          ),
+          SizedBox(
+            width: 0,
+            height: 20,
+          ),
+          ElevateButton(
+            title: "アラーム音設定",
+            width: 180,
+            height: 55,
+            fontSize: 20,
+            shape: 30,
+          )
+        ]),
       ),
     );
   }

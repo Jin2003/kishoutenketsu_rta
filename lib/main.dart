5a08b48cfe252ce8f73fdd2c858208f63e31b8cf
Fuuma <masa81hiro69@gmail.com>
2023-06-13 01:09:32 +0900

    [add]firebase_init

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:kishoutenketsu_rta/view/pages/start_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   // 画面の向きを固定
   // SystemChrome.setPreferredOrientations([
   //   DeviceOrientation.portraitUp,
   // ]);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }

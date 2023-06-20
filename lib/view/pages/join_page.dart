20c2ab885d14fcf33afd8c907fb7059403c1ddaa
Jin2003 <sakura0419gen@icloud.com>
2023-06-20 14:08:03 +0900

    [add] join_page

--- lib/view/pages/join_page.dart
+++ lib/view/pages/join_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../../logic/nav_bar.dart';
+import '../constant.dart';
+import 'components/custom_text.dart';
 
 class JoinPage extends StatefulWidget {
   const JoinPage({super.key});
@@ -10,6 +13,41 @@ class JoinPage extends StatefulWidget {
 class _JoinPageState extends State<JoinPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Constant.white,
+      body: Column(
+        children: [
+          const SizedBox(height: 40),
+          // × のアイコン表示
+          Align(
+            alignment: const Alignment(-0.9, 0),
+            child: IconButton(
+              iconSize: 22,
+              icon: const ImageIcon(
+                AssetImage('assets/icon/close_icon.png'),
+                color: Constant.gray,
+              ),
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: ((context) => const NavBar())),
+                );
+              },
+            ),
+          ),
+
+          const CustomText(text: 'グループ参加', fontSize: 22, Color: Constant.gray),
+          const SizedBox(height: 20),
+
+          Container(
+            width: 380,
+            decoration: const BoxDecoration(
+              border:
+                  Border(bottom: BorderSide(color: Constant.gray, width: 2)),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

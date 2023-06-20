a1b78a231b02190912d6c3099e65fc50ee6ca40f
Fuuma <masa81hiro69@gmail.com>
2023-06-20 16:26:37 +0900

    [add]firebase系をまとめるクラスを作成

---
+++ lib/logic/firebase_helper.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseHelper {
+  // グループを作成するメソッド
+  Future<String> createGroup() async {
+    DocumentReference docRef =
+        await FirebaseFirestore.instance.collection('groups').add({});
+    // 作成したドキュメントIDを取得
+    String documentID = docRef.id;
+    // ドキュメントIDに対してgroupIDフィールドを更新
+    await FirebaseFirestore.instance
+        .collection('groups')
+        .doc(documentID)
+        .update({
+      'groupID': documentID,
+    });
+    return documentID;
+  }
+
+  // グループにnfcIdListを保存するメソッド
+  Future<void> saveNfcIdList(String documentID, List<String> nfcIdList) async {
+    await FirebaseFirestore.instance
+        .collection('groups')
+        .doc(documentID)
+        .update({
+      'nfcIdList': nfcIdList,
+    });
+  }
+}

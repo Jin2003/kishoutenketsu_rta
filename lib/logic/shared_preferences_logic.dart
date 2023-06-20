5d0c0b0a657429ec045b3776e4f5fa30c0a81556
Fuuma <masa81hiro69@gmail.com>
2023-06-20 16:26:13 +0900

    [hotfix]インスタンスつくってなかった

--- lib/logic/shared_preferences_logic.dart
+++ lib/logic/shared_preferences_logic.dart
@@ -22,91 +22,109 @@ class SharedPreferencesLogic {
 
   // groupIDをローカルに保存
   Future<void> setGroupID(String groupID) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setString(keyGroupID, groupID);
   }
 
   // userIDをローカルに保存
   Future<void> setUserID(String userID) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setString(keyUserID, userID);
   }
 
   // 現在のポイントをローカルに保存
   Future<void> setCurrentPoint(int currentPoint) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setInt(keyCurrentPoint, currentPoint);
   }
 
   // アラームのON/OFFをローカルに保存
   Future<void> setSettedAlarm(bool settedAlarm) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setBool(keySettedAlarm, settedAlarm);
   }
 
   // NFCを登録しているかどうかをローカルに保存
   Future<void> setRegisteredNFC(bool registeredNFC) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setBool(keyRegisteredNFC, registeredNFC);
   }
 
   // 選択の音楽をローカルに保存
   Future<void> setSelectedMusic(String selectedMusic) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setString(keySelectedMusic, selectedMusic);
   }
 
   // 選択テーマをローカルに保存
   Future<void> setSelectedTheme(String selectedTheme) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setString(keySelectedTheme, selectedTheme);
   }
 
   // 選択キャラクターをローカルに保存
   Future<void> setSelectedCharacter(String selectedCharacter) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setString(keySelectedCharacter, selectedCharacter);
   }
 
   // userIDをローカルから取得
   Future<String?> getUserID() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getString(keyUserID);
   }
 
   // groupIDをローカルから取得
   Future<String?> getGroupID() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getString(keyGroupID);
   }
 
   // 現在のポイントをローカルから取得
   Future<int?> getCurrentPoint() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getInt(keyCurrentPoint);
   }
 
   // アラームのON/OFFをローカルから取得
   Future<bool?> getSettedAlarm() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getBool(keySettedAlarm);
   }
 
   // NFCを登録しているかどうかをローカルから取得
   Future<bool?> getRegisteredNFC() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getBool(keyRegisteredNFC);
   }
 
   // 選択の音楽をローカルから取得
   Future<String?> getSelectedMusic() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getString(keySelectedMusic);
   }
 
   // 選択テーマをローカルから取得
   Future<String?> getSelectedTheme() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getString(keySelectedTheme);
   }
 
   // 選択キャラクターをローカルから取得
   Future<String?> getSelectedCharacter() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getString(keySelectedCharacter);
   }
 
   // ローカルに保存されているデータを全て削除
   Future<void> clearAllData() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.clear();
   }
 
   // ローカルに保存されているデータを全て取得
   Future<Map<String, dynamic>> getAllData() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getKeys().fold<Map<String, dynamic>>(
         {}, (map, key) => map..[key] = sharedPreferences.get(key));
   }

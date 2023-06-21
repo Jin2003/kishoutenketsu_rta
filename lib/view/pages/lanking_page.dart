477b419ddf519359096128859021cf2a64a9a38b
AluFu <0hp3261131m102u@ezweb.ne.jp>
2023-06-21 23:39:15 +0900

    [add]chatgptから取得したテキストをニワトリの画像を押したら吹き出し上に表示するようにした

--- lib/view/pages/lanking_page.dart
+++ lib/view/pages/lanking_page.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:kishoutenketsu_rta/view/constant.dart';
 import '../../logic/database_helper.dart';
 import 'components/custom_text.dart';
+import 'package:kishoutenketsu_rta/logic/chatgpt_service.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
 
 class LankingPage extends StatefulWidget {
   const LankingPage({super.key});
@@ -16,10 +21,43 @@ class _LankingPageState extends State<LankingPage> {
   //Lankingを何個表示するか
   int _lankingCount = 0;
 
+    bool _showBubble = false;
+
+  //chatGPTへの入力を保持する配列
+  List<String> _message = [
+    "「ランキング更新！」だけ言ってくださいそれ以外は言わないでください",
+    "「今日のラッキーアイテム」だけ言ってくださいそれ以外は言わないでください",
+  ];
+  //０から３までのランダムな数字を保持する変数
+  int? _Random;
+
+  // ChatGPTからの応答を保持する変数
+  String? _response;
+  // ChatGPTの応答を表示するかどうかのフラグ
+  bool _showResponse = false;
+
+
   @override
   void initState() {
     super.initState();
     _loadLank();
+  }
+
+    void _toggleBubble() {
+    setState(() {
+      _showBubble = !_showBubble;
+    });
+  }
+
+  // ChatGPTからの応答を取得する関数
+  Future<void> _getChatGPTResponse() async {
+    final chatGPT = ChatGPT();
+    final response =
+        await chatGPT.message(_message[0]);
+    setState(() {
+      _response = response.content;
+      _showResponse = !_showResponse;
+    });
   }
 
   //_timeを取得する関数
@@ -71,38 +109,102 @@ class _LankingPageState extends State<LankingPage> {
                 )),
           ],
         ),
-        // 吹き出し
-        Align(
-          alignment: const Alignment(-0.5, 0.85),
-          child: SizedBox(
-            width: 250,
-            height: 190,
-            child: Image.asset(
-              "assets/speech_bubble.png",
+                 // 吹き出し
+          // Visibility(
+          //   visible: _showBubble,
+          //   child: Align(
+          //     alignment: const Alignment(-0.5, 0.85),
+          //     child: SizedBox(
+          //       width: 250,
+          //       height: 190,
+          //       child: Image.asset(
+          //         "assets/speech_bubble.png",
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          // 吹き出しの中身(ChatGPTの応答)
+          Visibility(
+            visible: _showResponse,
+            child: Align(
+              alignment: const Alignment(-0.4, 0.65),
+              // alignment: const Alignment(-0.25, 1.14),
+              child: Container(
+                margin: const EdgeInsets.only(left: 15.0),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 7.0,
+                  horizontal: 10.0,
+                ),            
+                child: Align(
+                  alignment: const Alignment(-0.4, 0.65),
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 15.0),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 7.0,
+                      horizontal: 10.0,
+                    ),
+                    decoration: ShapeDecoration(
+                      shape: BubbleBorder(
+                        width: 30,
+                        radius: 29,
+                      ),
+                    ),
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        TyperAnimatedText(
+                          _response ?? "",
+                          textStyle: GoogleFonts.zenMaruGothic(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: const Color(0xFF707070),
+                          ),
+                          speed: const Duration(milliseconds: 100),
+                        ),
+                      ],
+                      totalRepeatCount: 1,
+                    ),
+                  ),
+                ),
+              ),
+              // child: SizedBox(
+              //   width: 200,
+              //   height: 190,
+              //   child: AnimatedTextKit(
+              //     animatedTexts: [
+              //       TyperAnimatedText(_response ?? "",
+              //           textStyle: GoogleFonts.zenMaruGothic(
+              //             fontWeight: FontWeight.bold,
+              //             fontSize: 15,
+              //             color: Color(0xFF707070),
+              //           ),
+              //           speed: const Duration(milliseconds: 100)
+              //           ),
+              //     ],
+              //     totalRepeatCount:1,
+              //   ),
+              // ),
+              
             ),
           ),
-        ),
-        // TODO: ここにchatGPT
-        const Align(
-          alignment: Alignment(-0.40, 0.62),
-          child: CustomText(
-            text: '今日のラッキーアイテム',
-            fontSize: 18,
-            Color: Constant.gray,
-          ),
-        ),
-        // 鶏の画像
-        Align(
-          alignment: const Alignment(0.8, 0.95),
-          child: SizedBox(
-            width: 120,
-            height: 120,
-            child: Image.asset(
-              "assets/chicken.png",
+          GestureDetector(
+            onTap: () async{
+              await _getChatGPTResponse();
+              _toggleBubble();
+              
+            },
+            child: Align(
+              alignment: const Alignment(0.8, 0.95),
+              child: SizedBox(
+                width: 120,
+                height: 120,
+                child: Image.asset(
+                  "assets/chicken.png",
+                ),
+              ),
             ),
           ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
@@ -185,4 +287,79 @@ Widget _buildCard(int index, Map<String, dynamic> time) {
       ),
     ),
   );
+}
+
+//吹き出しの形を作るクラス
+class BubbleBorder extends ShapeBorder {
+
+  BubbleBorder({
+    required this.width,
+    required this.radius,
+  });
+
+  final double width;
+  final double radius;
+
+
+  @override
+  EdgeInsetsGeometry get dimensions {
+
+    return EdgeInsets.all(width);
+  }
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection? textDirection}) {
+
+    return getOuterPath(
+      rect.deflate(width / 2.0),
+      textDirection: textDirection,
+      
+    );
+  }
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
+    final r = radius;
+    final rs = radius / 2;
+    final w = rect.size.width; // 全体の横幅
+    final h = rect.size.height; // 全体の縦幅
+    final wl = w / 3; 
+    return Path()
+      ..addPath(
+        Path()
+          ..moveTo(r, 0)
+          ..lineTo(w - r - 2 , 0) // →
+          ..lineTo(w - r - 3 , 0) // →
+          ..arcToPoint(Offset(w - 5 , r), radius: Radius.circular(r + 6))
+          ..arcToPoint(Offset(w - r - 5, h + 1), radius: Radius.circular(r + 2),clockwise: true)
+          ..relativeLineTo(10 , 15)
+          ..arcToPoint(Offset(w - r * 1.8, h + 3), radius: Radius.circular(r * 6),clockwise: true)
+          ..lineTo(wl + r, h) // ←
+          ..lineTo(r, h) // ←
+          ..arcToPoint(Offset(0, h - r), radius: Radius.circular(r))
+          ..arcToPoint(Offset(r, 0), radius: Radius.circular(r)),
+        Offset(rect.left, rect.top),
+      )
+      ..close();
+  }
+
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {
+
+    final paint = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 4
+      ..color = Color.fromARGB(255, 255, 255, 255);      
+    canvas.drawPath(
+      getOuterPath(
+        rect.deflate(width / 2.0),
+        textDirection: textDirection,
+      ),
+      paint,
+    );
+  }
+
+  @override
+  ShapeBorder scale(double t) => this;
 }
--- lib/view/pages/main_page.dart
+++ lib/view/pages/main_page.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kishoutenketsu_rta/view/pages/components/custom_text.dart';
 import '../constant.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:kishoutenketsu_rta/logic/chatgpt_service.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 import 'alarm_page.dart';
 
@@ -20,10 +24,41 @@ class _MainPageState extends State<MainPage> {
   // アラームオンオフの切り替え
   bool _value = true;
 
+  bool _showBubble = false;
+
+  //chatGPTへの入力を保持する配列
+  List<String> _message = [
+    "「りんごって美味しいよね！」だけ言ってくださいそれ以外は言わないでください",
+  ];
+  //０から３までのランダムな数字を保持する変数
+  int? _Random;
+
+  // ChatGPTからの応答を保持する変数
+  String? _response;
+  // ChatGPTの応答を表示するかどうかのフラグ
+  bool _showResponse = false;
+
   @override
   void initState() {
     _timeOfDay = const TimeOfDay(hour: 0, minute: 0);
     super.initState();
+  }
+
+    void _toggleBubble() {
+    setState(() {
+      _showBubble = !_showBubble;
+    });
+  }
+
+  // ChatGPTからの応答を取得する関数
+  Future<void> _getChatGPTResponse() async {
+    final chatGPT = ChatGPT();
+    final response =
+        await chatGPT.message(_message[0]);
+    setState(() {
+      _response = response.content;
+      _showResponse = !_showResponse;
+    });
   }
 
   @override
@@ -110,29 +145,96 @@ class _MainPageState extends State<MainPage> {
             ],
           ),
           // 吹き出し
-          Align(
-            alignment: const Alignment(-0.5, 0.85),
-            child: SizedBox(
-              width: 250,
-              height: 190,
-              child: Image.asset(
-                "assets/speech_bubble.png",
+          // Visibility(
+          //   visible: _showBubble,
+          //   child: Align(
+          //     alignment: const Alignment(-0.5, 0.85),
+          //     child: SizedBox(
+          //       width: 250,
+          //       height: 190,
+          //       child: Image.asset(
+          //         "assets/speech_bubble.png",
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          // 吹き出しの中身(ChatGPTの応答)
+          Visibility(
+            visible: _showResponse,
+            child: Align(
+              alignment: const Alignment(-0.4, 0.65),
+              // alignment: const Alignment(-0.25, 1.14),
+              child: Container(
+                margin: const EdgeInsets.only(left: 15.0),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 7.0,
+                  horizontal: 10.0,
+                ),            
+                child: Align(
+                  alignment: const Alignment(-0.4, 0.65),
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 15.0),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 7.0,
+                      horizontal: 10.0,
+                    ),
+                    decoration: ShapeDecoration(
+                      shape: BubbleBorder(
+                        width: 30,
+                        radius: 29,
+                      ),
+                    ),
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        TyperAnimatedText(
+                          _response ?? "",
+                          textStyle: GoogleFonts.zenMaruGothic(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: const Color(0xFF707070),
+                          ),
+                          speed: const Duration(milliseconds: 100),
+                        ),
+                      ],
+                      totalRepeatCount: 1,
+                    ),
+                  ),
+                ),
               ),
+              // child: SizedBox(
+              //   width: 200,
+              //   height: 190,
+              //   child: AnimatedTextKit(
+              //     animatedTexts: [
+              //       TyperAnimatedText(_response ?? "",
+              //           textStyle: GoogleFonts.zenMaruGothic(
+              //             fontWeight: FontWeight.bold,
+              //             fontSize: 15,
+              //             color: Color(0xFF707070),
+              //           ),
+              //           speed: const Duration(milliseconds: 100)
+              //           ),
+              //     ],
+              //     totalRepeatCount:1,
+              //   ),
+              // ),
+              
             ),
           ),
-          // TODO: ここにchatGPT
-          const Align(
-            alignment: Alignment(-0.40, 0.62),
-            child: CustomText(text:'今日のラッキーアイテム', fontSize: 18, Color: Constant.gray,),
-          ),
-          // 鶏の画像
-          Align(
-            alignment: const Alignment(0.8, 0.95),
-            child: SizedBox(
-              width: 120,
-              height: 120,
-              child: Image.asset(
-                "assets/chicken.png",
+          GestureDetector(
+            onTap: () async{
+              await _getChatGPTResponse();
+              _toggleBubble();
+              
+            },
+            child: Align(
+              alignment: const Alignment(0.8, 0.95),
+              child: SizedBox(
+                width: 120,
+                height: 120,
+                child: Image.asset(
+                  "assets/chicken.png",
+                ),
               ),
             ),
           ),
@@ -181,3 +283,80 @@ class _alarmSelectorDialog extends StatelessWidget {
     );
   }
 }
+
+
+//吹き出しの形を作るクラス
+class BubbleBorder extends ShapeBorder {
+
+  BubbleBorder({
+    required this.width,
+    required this.radius,
+  });
+
+  final double width;
+  final double radius;
+
+
+  @override
+  EdgeInsetsGeometry get dimensions {
+
+    return EdgeInsets.all(width);
+  }
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection? textDirection}) {
+
+    return getOuterPath(
+      rect.deflate(width / 2.0),
+      textDirection: textDirection,
+      
+    );
+  }
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
+    final r = radius;
+    final rs = radius / 2;
+    final w = rect.size.width; // 全体の横幅
+    final h = rect.size.height; // 全体の縦幅
+    final wl = w / 3; 
+    return Path()
+      ..addPath(
+        Path()
+          ..moveTo(r, 0)
+          ..lineTo(w - r - 2 , 0) // →
+          ..lineTo(w - r - 3 , 0) // →
+          ..arcToPoint(Offset(w - 5 , r), radius: Radius.circular(r + 6))
+          ..arcToPoint(Offset(w - r - 5, h + 1), radius: Radius.circular(r + 2),clockwise: true)
+          ..relativeLineTo(10 , 15)
+          ..arcToPoint(Offset(w - r * 1.8, h + 3), radius: Radius.circular(r * 6),clockwise: true)
+          ..lineTo(wl + r, h) // ←
+          ..lineTo(r, h) // ←
+          ..arcToPoint(Offset(0, h - r), radius: Radius.circular(r))
+          ..arcToPoint(Offset(r, 0), radius: Radius.circular(r)),
+        Offset(rect.left, rect.top),
+      )
+      ..close();
+  }
+
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {
+
+    final paint = Paint()
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 4
+      ..color = Color.fromARGB(255, 255, 255, 255);      
+    canvas.drawPath(
+      getOuterPath(
+        rect.deflate(width / 2.0),
+        textDirection: textDirection,
+      ),
+      paint,
+    );
+  }
+
+  @override
+  ShapeBorder scale(double t) => this;
+}
+

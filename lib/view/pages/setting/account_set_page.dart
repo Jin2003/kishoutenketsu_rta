161d19b13d2a772d2ea570996f431add1321b37f
Fuuma <masa81hiro69@gmail.com>
2023-06-21 15:25:41 +0900

    [add]ユーザIDとメールアドレスをfirebaseから持ってくる

--- lib/view/pages/setting/account_set_page.dart
+++ lib/view/pages/setting/account_set_page.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:kishoutenketsu_rta/logic/nav_bar.dart';
+import 'package:kishoutenketsu_rta/logic/shared_preferences_logic.dart';
 import 'package:kishoutenketsu_rta/view/constant.dart';
 import 'package:kishoutenketsu_rta/view/pages/components/custom_text.dart';
 import 'package:kishoutenketsu_rta/view/pages/setting/email_set_page.dart';
@@ -14,6 +17,31 @@ class AccountSetPage extends StatefulWidget {
 }
 
 class _AccountSetPageState extends State<AccountSetPage> {
+  dynamic userData;
+  String? userEmail;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
+
+  Future<void> fetchUserData() async {
+    // ユーザIDを取得
+    SharedPreferencesLogic sharedPreferencesLogic = SharedPreferencesLogic();
+    final userID = await sharedPreferencesLogic.getUserID();
+    final userDoc =
+        await FirebaseFirestore.instance.collection('users').doc(userID).get();
+    userData = userDoc.data();
+
+    // ユーザーのメールアドレスを取得
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      userEmail = user.email;
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,10 +78,14 @@ class _AccountSetPageState extends State<AccountSetPage> {
             ),
           ),
           const SizedBox(height: 10),
-          // TODO: ここに今設定しているニックネームの表示
-          _ListTileWidget(context, 'ニックネーム' , 'Fuuma', const NicknameSetPage()),
-          _ListTileWidget(context, 'メールアドレス' , 'fuuma@masa.hiro', const EmailSetPage()),
-          _ListTileWidget(context, 'パスワード' , '●●●●●●', const PwdSetPage()),
+          _ListTileWidget(
+              context,
+              'ニックネーム',
+              userData != null ? userData['name'] : '',
+              const NicknameSetPage()),
+          _ListTileWidget(
+              context, 'メールアドレス', userEmail ?? '', const EmailSetPage()),
+          _ListTileWidget(context, 'パスワード', '●●●●●●', const PwdSetPage()),
         ],
       ),
     );
@@ -62,18 +94,20 @@ class _AccountSetPageState extends State<AccountSetPage> {
 
 // ListTileを表示するウィジェット
 // ignore: non_constant_identifier_names
-Widget _ListTileWidget(context, String title, String currentSetting, Widget nextPage) {
+Widget _ListTileWidget(
+    context, String title, String currentSetting, Widget nextPage) {
   return SizedBox(
     width: 380,
     child: ListTile(
-      contentPadding: const EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 10),
+      contentPadding:
+          const EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 10),
       title: Align(
           alignment: const Alignment(-0.9, 0),
-          child:
-              CustomText(text: title, fontSize: 16, Color: Constant.gray)),
+          child: CustomText(text: title, fontSize: 16, Color: Constant.gray)),
       subtitle: Align(
           alignment: const Alignment(-0.9, 0),
-          child: CustomText(text: currentSetting, fontSize: 23, Color: Constant.gray)),
+          child: CustomText(
+              text: currentSetting, fontSize: 23, Color: Constant.gray)),
       trailing: const ImageIcon(
         AssetImage('assets/icon/r_arrow_icon.png'),
         color: Constant.gray,

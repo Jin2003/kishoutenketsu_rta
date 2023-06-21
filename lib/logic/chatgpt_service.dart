84a28271b0eaedafa458195ad8032e8404bc0db5
AluFu <0hp3261131m102u@ezweb.ne.jp>
2023-06-21 23:40:28 +0900

    [create] new file

---
+++ lib/logic/chatgpt_service.dart
@@ -0,0 +1,48 @@
+import 'package:dart_openai/dart_openai.dart';
+
+class ChatGPT{
+
+  
+  message(String message) async {
+    
+  //アシスタント調整用メッセージ
+  // await OpenAI.instance.chat.create(
+  //   model: "gpt-3.5-turbo",
+  //   messages: [
+  //     const OpenAIChatCompletionChoiceMessageModel(
+  //       content:'''
+  //                 あなたは質問文を繰り返すだけのアシスタントです。
+
+  //                 考慮事項
+  //                 - 日本語で答えてください
+  //               '''
+  //       ,
+  //       role: OpenAIChatMessageRole.system,
+  //     )
+  //   ]
+  // );
+
+  //ユーザーメッセージ
+  // await OpenAI.instance.chat.create(
+  //     model: "gpt-3.5-turbo",
+  //     messages: [
+  //       OpenAIChatCompletionChoiceMessageModel(
+  //         content: message1,
+  //         role: OpenAIChatMessageRole.user,
+  //       )
+  //     ]
+  //   );
+
+    //アシスタントメッセージ
+    final response = await OpenAI.instance.chat.create(
+      model: "gpt-3.5-turbo",
+      messages: [
+        OpenAIChatCompletionChoiceMessageModel(
+          content: message,
+          role: OpenAIChatMessageRole.assistant,
+        )
+      ]
+    );
+    return response.choices.first.message;
+  }
+}

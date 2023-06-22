2629b38f7e5ea3a00ca8eebf520840889679a660
Jin2003 <sakura0419gen@icloud.com>
2023-06-22 20:52:37 +0900

    [add] constant.dart カラー追加 [update] テーマカラー変更ボタンのimage画質あげた

--- lib/view/constant.dart
+++ lib/view/constant.dart
@@ -2,19 +2,23 @@ import 'package:flutter/material.dart';
 
 // 定数を管理するクラス
 class Constant {
-  // ignore: constant_identifier_names
-  static const Color blue = Color.fromARGB(255, 89, 216, 212);
-  // ignore: constant_identifier_names
-  static const Color subBlue = Color(0xffc8ffff);
-  // ignore: constant_identifier_names
+  // gray
+  static const Color gray = Color(0xFF9C958F);
+  // white
   static const Color white = Colors.white;
 
+  // yellow
   static const Color yellow = Color(0xFFFFE475);
-  // yellowが薄くて文字が見えない時に使うyellowのちょっと濃いver.
-  static const Color accentYellow = Color(0xFFFFDC4F);
+  static const Color accentYellow = Color(0xFFFFDC4F); // yellowが薄くて文字が見えない時に使うyellowのちょっと濃いver.
   static const Color subYellow = Color(0xFFFFF9DD);
 
-  static const Color gray = Color(0xFF9C958F);
+  // blue
+  static const Color blue = Color(0xFF65E4E4);
+  static const Color subBlue = Color(0xFFC8FFFF);
+
+  // red
+  static const Color red = Color(0xFFFFB6A6);
+  static const Color subRed = Color(0xFFFFEAE5);
 
   static const Color main = yellow;
   static const Color sub = subYellow;

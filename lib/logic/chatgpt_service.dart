adbc05a816fe3024aa6d970bacbfd75b8230b74f
AluFu <0hp3261131m102u@ezweb.ne.jp>
2023-06-24 12:06:48 +0900

    [update]roleの値と調整用メッセージの内容修正

--- lib/logic/chatgpt_service.dart
+++ lib/logic/chatgpt_service.dart
@@ -1,49 +1,37 @@
 import 'package:dart_openai/dart_openai.dart';
 
-class ChatGPT{
-
-  
+class ChatGPT {
   Future<String> message(String message) async {
-    
-  //ユーザーメッセージ
-  // await OpenAI.instance.chat.create(
-  //     model: "gpt-3.5-turbo",
-  //     messages: [
-  //       OpenAIChatCompletionChoiceMessageModel(
-  //         content: message1,
-  //         role: OpenAIChatMessageRole.user,
-  //       )
-  //     ]
-  //   );
-
     try {
-      //アシスタント調整用メッセージ
-      await OpenAI.instance.chat.create(
-        model: "gpt-3.5-turbo",
-        messages: [
-          const OpenAIChatCompletionChoiceMessageModel(
-            content:'''
-                      - 日本語で答えてください
-                      - 「」の中のメッセージのみ言ってください
-                    '''
-            ,
-            role: OpenAIChatMessageRole.system,
-          )
-        ]
-      );
+      // システムメッセージ
+      final systemMessage = '''
+        考慮事項:
+        - 日本語で答えてください。
+        - とだけ言ってくださいという命令が来たらその前のメッセージを言ってください。
+        - ...の部分は適当に保管してください
+        - 歌ってという命令が来た時は♪をつけて短文で歌ってください
+        - 、。は使わないでください
+      ''';
 
-
-      //アシスタントメッセージ
+      // アシスタントのメッセージ
       final response = await OpenAI.instance.chat.create(
         model: "gpt-3.5-turbo",
         messages: [
           OpenAIChatCompletionChoiceMessageModel(
+            content: systemMessage,
+            role: OpenAIChatMessageRole.system,
+          ),
+          OpenAIChatCompletionChoiceMessageModel(
             content: message,
-            role: OpenAIChatMessageRole.assistant,
-          )
+            role: OpenAIChatMessageRole.user,
+          ),
         ],
       );
-      return response.choices.first.message.content;
+
+      // アシスタントの返答を取得
+      final assistantReply = response.choices.last.message.content;
+
+      return assistantReply;
     } catch (e) {
       // エラーメッセージを表示するなどの適切な処理を行う
       print("An error occurred: $e");

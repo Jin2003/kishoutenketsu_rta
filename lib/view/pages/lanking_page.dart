5d92806ab48b54f40ac027633af73daab40439e7
AluFu <0hp3261131m102u@ezweb.ne.jp>
2023-09-16 21:59:40 +0900

    [delete]一部コメントアウトを削除

--- lib/view/pages/lanking_page.dart
+++ lib/view/pages/lanking_page.dart
@@ -20,8 +20,14 @@ class _LankingPageState extends State<LankingPage> {
   //Lankingを何個表示するか
   int _lankingCount = 0;
 
+  //ランキングで自分の名前を保持する変数
+  String? _userName;
+
   //最新のRTAタイムを保持する変数
   Map<String, dynamic>? _rtaTime ;
+
+  //最高記録のRTAタイムを保持する変数
+  Map<String, dynamic>? _topTime;
 
   //TODOchatGPTへの入力を保持する配列
   // List<String> _message = [
@@ -38,7 +44,7 @@ class _LankingPageState extends State<LankingPage> {
   final List<String> _message = [
     "更新おめでとう！\n今日も一日頑張ろう!",
     "惜しい！\n",
-    "RTAをやってみよう!",
+    "タイムを競い合おう!",
     // "惜しい！\nあと秒で更新だったね!",
   ];
 
@@ -74,13 +80,27 @@ class _LankingPageState extends State<LankingPage> {
     List<Map<String, dynamic>> rtaResults =
         await FirebaseHelper().getRtaResults();
 
-    // 最新のRTAタイムを取得
-    if (rtaResults.isNotEmpty) {
-      Map<String, dynamic> rtaTime = rtaResults.last;
+        print('rtaResults: $rtaResults');
 
-      setState(() {
-        _rtaTime = rtaTime;
-      });
+    if (rtaResults.isNotEmpty) {
+      // rtaResultsの最新で登録されたタイムの名前を取得
+      final userName = rtaResults.last['name'];
+
+      print('userName: $userName');
+      
+      // 自分の最新のタイムを取得
+      final rtaTime = rtaResults.lastWhere(
+        (element) => element['name'] == userName,
+        orElse: () => <String, dynamic>{});
+
+          print('rtaTime: $rtaTime');
+
+      if (mounted) {
+        setState(() {
+          _rtaTime = rtaTime;
+          _userName = userName;
+        });
+      }
     }
 
     // ソート
@@ -89,48 +109,67 @@ class _LankingPageState extends State<LankingPage> {
     // ランキングの数を設定
     int lankingCount = rtaResults.length;
 
-    setState(() {
-      _lankingCount = lankingCount;
-    });
+    if(mounted){
+      setState(() {
+        _lankingCount = lankingCount;
+      });
+    }
     _result = rtaResults;
 
-    _getMessage();
+    _getMessage(_userName!);
   }
 
-  //定型文を表示する関数
-  _getMessage() async {
-    if (_rtaTime != null && _result.isNotEmpty) {
-      // ランキング最高記録のRTAタイムを取得
-      final topTime = _result.first;
-      // 最高記録のタイムと自分の最新のタイムの差を計算
-      final difference = _rtaTime!['rtaResult'] - topTime['rtaResult'];
+//定型文を表示する関数
+_getMessage(String userName) async{
+  // ランキング最高記録のRTAタイムを取得
+  _topTime = _result.firstWhere(
+      (element) => element['name'] == userName,
+      orElse: () => <String, dynamic>{}); 
 
-      if (difference >= 0) {
-        final minutes = difference ~/ 60;
-        final seconds = difference % 60 + 1;
+  print('topTime: $_topTime');
 
+  //自分の最新のタイムと最高記録のタイムの差を計算
+  final difference = (_rtaTime != null && _topTime != null)
+      ? _rtaTime!['rtaResult'] - _topTime!['rtaResult']
+      : 'タイムが取得できなかったよ'; 
+
+  print('${_rtaTime!['rtaResult']}だよ！');
+  print('${_topTime!['rtaResult']}だよ！');
+
+  if (_rtaTime != null && _result.isNotEmpty) {
+    if (difference != null && difference >= 0) {
+      final minutes = difference ~/ 60;
+      final seconds = difference % 60 + 1;
+
+      if(mounted){
         setState(() {
-          if(minutes == 0){
-            _response = "${_message[1]}あと $seconds 秒更新だったね!";
-          }else{
+          if (minutes == 0) {
+            _response = "${_message[1]}あと $seconds 秒で更新だったね!";
+          } else {
             _response = "${_message[1]}あと $minutes 分 $seconds 秒で更新だったね!";
           }
           _showResponse = !_showResponse;
         });
-      } else {
+      }
+    } else {
+      if(mounted){
         setState(() {
           _response = _message[0];
           _showResponse = !_showResponse;
         });
       }
-    } else {
+    }
+  } else {
+    if(mounted){
       setState(() {
         _response = _message[2];
         _showResponse = !_showResponse;
       });
     }
   }
+}
 
+  //定型文を表示する関数
   @override
   Widget build(BuildContext context) {
     return Scaffold(

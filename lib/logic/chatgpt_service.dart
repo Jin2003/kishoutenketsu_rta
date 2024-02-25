24d12a5bb4ea5cda97cf762c88b375ed099c4ba2
Fuuma <masa81hiro69@gmail.com>
2024-02-25 14:49:07 +0900

    feat: いらないらしいのでコメントアウト

--- lib/logic/chatgpt_service.dart
+++ lib/logic/chatgpt_service.dart
@@ -1,41 +1,41 @@
-import 'package:dart_openai/dart_openai.dart';
+// import 'package:dart_openai/dart_openai.dart';
 
-class ChatGPT {
-  Future<String> message(String message) async {
-    try {
-      // システムメッセージ
-      final systemMessage = '''
-        考慮事項:
-        - 日本語で答えてください。
-        - とだけ言ってくださいという命令が来たらその前のメッセージを言ってください。
-        - ...の部分は適当に保管してください
-        - 歌ってという命令が来た時は♪をつけて短文で歌ってください
-        - 、。は使わないでください
-      ''';
+// class ChatGPT {
+//   Future<String> message(String message) async {
+//     try {
+//       // システムメッセージ
+//       // final systemMessage = '''
+//       //   考慮事項:
+//       //   - 日本語で答えてください。
+//       //   - とだけ言ってくださいという命令が来たらその前のメッセージを言ってください。
+//       //   - ...の部分は適当に保管してください
+//       //   - 歌ってという命令が来た時は♪をつけて短文で歌ってください
+//       //   - 、。は使わないでください
+//       // ''';
 
-      // アシスタントのメッセージ
-      final response = await OpenAI.instance.chat.create(
-        model: "gpt-3.5-turbo",
-        messages: [
-          OpenAIChatCompletionChoiceMessageModel(
-            content: systemMessage,
-            role: OpenAIChatMessageRole.system,
-          ),
-          OpenAIChatCompletionChoiceMessageModel(
-            content: message,
-            role: OpenAIChatMessageRole.user,
-          ),
-        ],
-      );
+//       // // アシスタントのメッセージ
+//       // final response = await OpenAI.instance.chat.create(
+//       //   model: "gpt-3.5-turbo",
+//       //   messages: [
+//       //     OpenAIChatCompletionChoiceMessageModel(
+//       //       content: systemMessage,
+//       //       role: OpenAIChatMessageRole.system,
+//       //     ),
+//       //     OpenAIChatCompletionChoiceMessageModel(
+//       //       content: message,
+//       //       role: OpenAIChatMessageRole.user,
+//       //     ),
+//       //   ],
+//       // );
 
-      // アシスタントの返答を取得
-      final assistantReply = response.choices.last.message.content;
+//       // // アシスタントの返答を取得
+//       // final assistantReply = response.choices.last.message.content;
 
-      return assistantReply;
-    } catch (e) {
-      // エラーメッセージを表示するなどの適切な処理を行う
-      print("An error occurred: $e");
-      return "やっほ〜";
-    }
-  }
-}
+//       // return assistantReply;
+//     } catch (e) {
+//       // エラーメッセージを表示するなどの適切な処理を行う
+//       print("An error occurred: $e");
+//       return "やっほ〜";
+//     }
+//   }
+// }
